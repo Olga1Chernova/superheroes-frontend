dbe9dbdbdb9734cc33d0292538301fac5c895c25
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-05-31 18:14:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,7 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import MainPage from 'pages/MainPage/MainPage';
 import Header from '../components/Header/Header';
-import AdditionalInfoPage from 'pages/AdditionalInfoPage/AdditionalInfoPage';
-import EditInfoFormPage from 'pages/EditInfoFormPage/EditInfoFormPage';
 
 const MainPage = lazy(() => import('pages/MainPage/MainPage'));
 const AdditionalInfoPage = lazy(() => import('pages/AdditionalInfoPage/AdditionalInfoPage'));
